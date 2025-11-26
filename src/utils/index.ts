329cbb9a6472cb27f01b3ee3c434935e2fcc0cbc
kishor <kishor.i@refurbedge.com>
2025-11-26 13:06:43 +0530

    fix(utils): correct response-handler.utils export typo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export * from './logger.utils';
 export * from './password.utils';
-export * from './prisma.utils';
+export * from './prisma.utils1';
 export * from './redis.utils1';
 export * from './response-handler.utilss';
